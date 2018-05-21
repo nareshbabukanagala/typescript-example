87411858af6be7169a41e87bd35451b05c614cf8
nareshbabukanagala <protractortooltraining@gmail.com>
2018-05-21 20:44:11 +0530

    05/21/2018 coverting .xlsx files to json files from folder

--- conf.ts
+++ conf.ts
@@ -1,12 +1,34 @@
 
 import {Config} from 'protractor';
+const xlsxj = require("xlsx-2-json");
+const testFolder = './testData/';
+const fs = require('fs');
+
+fs.readdirSync(testFolder).forEach(file => {
+    console.log(file);
+    if (file.includes('xlsx')) {
+        var n = file.split('.');
+        console.log(n[0])
+        xlsxj({
+            input: "./testData/" + n[0] + ".xlsx",
+            output: "./testData/" + n[0] + ".json",
+            sheet: "Sheet2"
+        }, function(err, result) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(result);
+            }
+        });
+    }
+})
 
 export let config: Config = {
   framework: 'jasmine2',
   capabilities: {
     browserName: 'chrome'
   },
-  specs: [ './specs/specPageObjects.js' ],
+  specs: [ './specs/spec.js' ],
   onPrepare:()=>{
   //  var gvar='global variable in onprepare section';
    
